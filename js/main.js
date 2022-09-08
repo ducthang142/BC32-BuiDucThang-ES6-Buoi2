eb83db10e7ec87cb43bbe5fe1edaae9253db0b7c
ducthang142 <anubin142@gmail.com>
2022-09-08 23:56:24 +0700

    note

--- js/main.js
+++ js/main.js
@@ -1,12 +1,146 @@
 let dataGlasses = [
-    { id: "G1", src: "./img/g1.jpg", virtualImg: "./img/v1.png", brand: "Armani Exchange", name: "Bamboo wood", color: "Brown", price: 150, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? " },
-    { id: "G2", src: "./img/g2.jpg", virtualImg: "./img/v2.png", brand: "Arnette", name: "American flag", color: "American flag", price: 150, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio." },
-    { id: "G3", src: "./img/g3.jpg", virtualImg: "./img/v3.png", brand: "Burberry", name: "Belt of Hippolyte", color: "Blue", price: 100, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
-    { id: "G4", src: "./img/g4.jpg", virtualImg: "./img/v4.png", brand: "Coarch", name: "Cretan Bull", color: "Red", price: 100, description: "In assumenda earum eaque doloremque, tempore distinctio." },
-    { id: "G5", src: "./img/g5.jpg", virtualImg: "./img/v5.png", brand: "D&G", name: "JOYRIDE", color: "Gold", price: 180, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?" },
-    { id: "G6", src: "./img/g6.jpg", virtualImg: "./img/v6.png", brand: "Polo", name: "NATTY ICE", color: "Blue, White", price: 120, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
-    { id: "G7", src: "./img/g7.jpg", virtualImg: "./img/v7.png", brand: "Ralph", name: "TORTOISE", color: "Black, Yellow", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam." },
-    { id: "G8", src: "./img/g8.jpg", virtualImg: "./img/v8.png", brand: "Polo", name: "NATTY ICE", color: "Red, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim." },
-    { id: "G9", src: "./img/g9.jpg", virtualImg: "./img/v9.png", brand: "Coarch", name: "MIDNIGHT VIXEN REMIX", color: "Blue, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet." }
+  {
+    id: "G1",
+    src: "./img/g1.jpg",
+    virtualImg: "./img/v1.png",
+    brand: "Armani Exchange",
+    name: "Bamboo wood",
+    color: "Brown",
+    price: 150,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? ",
+  },
+  {
+    id: "G2",
+    src: "./img/g2.jpg",
+    virtualImg: "./img/v2.png",
+    brand: "Arnette",
+    name: "American flag",
+    color: "American flag",
+    price: 150,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G3",
+    src: "./img/g3.jpg",
+    virtualImg: "./img/v3.png",
+    brand: "Burberry",
+    name: "Belt of Hippolyte",
+    color: "Blue",
+    price: 100,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G4",
+    src: "./img/g4.jpg",
+    virtualImg: "./img/v4.png",
+    brand: "Coarch",
+    name: "Cretan Bull",
+    color: "Red",
+    price: 100,
+    description: "In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G5",
+    src: "./img/g5.jpg",
+    virtualImg: "./img/v5.png",
+    brand: "D&G",
+    name: "JOYRIDE",
+    color: "Gold",
+    price: 180,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?",
+  },
+  {
+    id: "G6",
+    src: "./img/g6.jpg",
+    virtualImg: "./img/v6.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Blue, White",
+    price: 120,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G7",
+    src: "./img/g7.jpg",
+    virtualImg: "./img/v7.png",
+    brand: "Ralph",
+    name: "TORTOISE",
+    color: "Black, Yellow",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam.",
+  },
+  {
+    id: "G8",
+    src: "./img/g8.jpg",
+    virtualImg: "./img/v8.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Red, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim.",
+  },
+  {
+    id: "G9",
+    src: "./img/g9.jpg",
+    virtualImg: "./img/v9.png",
+    brand: "Coarch",
+    name: "MIDNIGHT VIXEN REMIX",
+    color: "Blue, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet.",
+  },
 ];
 
+//Gọi hàm display để hiển thị kính ra bên trái màn hình
+display(dataGlasses);
+
+//Hàm display để hiển thị kính lên khung bên trái
+function display(dataGlasses) {
+  let html = dataGlasses.reduce((result, glass) => {
+    return (
+      result +
+      `
+        <div class="col-4">
+            <img src="${glass.src}" width="150px" height="auto" data-id="${glass.id}"/>
+        </div>
+        `
+    );
+  }, "");
+
+  dom("#vglassesList").innerHTML = html;
+}
+
+//Hàm DOM
+function dom(selector) {
+  return document.querySelector(selector);
+}
+
+//==================================================================================
+
+//Lắng nghe sự kiện click vào kính bên bảng bên trái
+dom("#vglassesList").addEventListener("click", (evt) => {
+  let id = evt.target.getAttribute("data-id");
+
+  //Tìm index trong mảng dataGlasses của kính dc click thông qua id
+  let index = dataGlasses.findIndex((glass) => {
+    return glass.id === id;
+  });
+
+  //Hiển thị ra trên người mẫu bên phải
+  displayModal(dataGlasses[index]);
+});
+
+//Hàm hiển thị kính lên người mẫu ở bên phải
+function displayModal(glass) {
+  let html = `
+    <img src="${glass.virtualImg}" width="150px" height="auto" />
+    `;
+
+  dom("#avatar").innerHTML = html;
+}
